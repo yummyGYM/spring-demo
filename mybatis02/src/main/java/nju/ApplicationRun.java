7f75b570371469ee79df8245ecbaecb6e19dae09
yummy_gym <yummy_gym@163.com>
2019-09-07 14:19:16 +0800

    111

---
+++ mybatis02/src/main/java/nju/ApplicationRun.java
@@ -0,0 +1,13 @@
+package nju;
+
+/**
+ * @Author: Gu Yaming
+ * @Description:
+ * @Date:Create：in 2019/9/7 14:13
+ * @Modified By：
+ */
+public class ApplicationRun {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
